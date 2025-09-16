5f377c139aec99fd76492ce7212422b9f69de4d3
Chadni Rathi <chandani.rathi@gmail.com>
2025-09-16 14:38:36 +0530

    update vitest config

--- packages/ripple-router-hash/vitest.config.js
+++ packages/ripple-router-hash/vitest.config.js
@@ -1,13 +1,24 @@
 import { mergeConfig, configDefaults, defineConfig } from 'vitest/config';
-import viteConfig from './vite.config'
+import viteConfig from './vite.config';
+import { ripple } from 'vite-plugin-ripple';
+import path from 'node:path';
 
-
-export default mergeConfig(viteConfig, defineConfig({
+export default defineConfig({
+	plugins: [ripple({})],
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+		},
+		extensions: ["js"]
+	},
 	test: {
-		include: [
-			'tests/*.test.ripple',
-		],
+		include: ['tests/*.test.ripple'],
 		environment: 'jsdom',
 		...configDefaults.test,
 	},
-}))
+	build: {
+		rollupOptions: {
+			external: ['virtual:ripple-routes'],
+		},
+	},
+});
