8608582f13f3823ab29f4848a25b6de3969bd62f
Chadni Rathi <chandani.rathi@gmail.com>
2025-09-17 08:52:25 +0530

    fixed  vitest issue

---
+++ packages/ripple-router-hash/vitest.config.js
@@ -0,0 +1,25 @@
+import { configDefaults, defineConfig } from 'vitest/config';
+import { ripple } from 'vite-plugin-ripple';
+import path from 'node:path';
+
+export default defineConfig({
+	plugins: [ripple({})],
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+		},
+	},
+	test: {
+		include: ['tests/*.test.ts','tests/*.test.ripple'],
+		deps: {
+			inline: ['ripple'],
+		},
+		environment: 'jsdom',
+		...configDefaults.test,
+	},
+	build: {
+		rollupOptions: {
+			external: ['virtual:ripple-routes'],
+		},
+	},
+});
