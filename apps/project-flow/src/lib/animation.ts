b872e094cd0c1fa1627c3d11ce41ba9d2bfbd816
Chadni Rathi <chandani.rathi@gmail.com>
2025-09-21 20:04:03 +0530

    fixed ripple update issue

--- apps/project-flow/src/lib/animation.ts
+++ apps/project-flow/src/lib/animation.ts
@@ -1,11 +1,24 @@
-
+import { effect } from "ripple";
 import { animate } from "motion"
 
 export function slideInSlideOut(options) {
-    return node => {
-        animate(node, { opacity: [0.8, 1], x: [node.clientWidth, 0] }, { duration: 0.4 })
+    let _node;
+    effect(() => {
+        console.log("animation enter effect")
+        animate(_node, { opacity: [0.8, 1], x: [_node.clientWidth, 0] }, { duration: 0.4 })
         return () => {
-            return animate(node, { opacity: [1, 0.8, 0.6, 0], x: [0, -node.clientWidth] }, { duration: 0.4 })
+            console.log("animation exist effect")
+            animate(_node, { opacity: [1, 0.8, 0.6, 0], x: [0, -_node.clientWidth] }, { duration: 0.4 })
+        }
+    })
+
+    return node => {
+        console.log("animation mounted")
+        _node = node;
+        
+        return () => {
+            console.log("animation unmounted")
+            _node = undefined 
         }
     }
 }
